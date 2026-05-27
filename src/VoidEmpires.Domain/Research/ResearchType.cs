faf4c2d35dc7c9c36587f662bf2ff6d269f8072e
devRaGonSa <97627393+devRaGonSa@users.noreply.github.com>
2026-05-27 20:28:32 +0200

    feat(domain): add research type model

---
+++ src/VoidEmpires.Domain/Research/ResearchType.cs
@@ -0,0 +1,13 @@
+namespace VoidEmpires.Domain.Research;
+
+public enum ResearchType
+{
+    PlanetaryEngineering = 1,
+    ResourceExtraction = 2,
+    EnergySystems = 3,
+    ConstructionAutomation = 4,
+    Propulsion = 5,
+    ShipWeapons = 6,
+    Shielding = 7,
+    Espionage = 8
+}
