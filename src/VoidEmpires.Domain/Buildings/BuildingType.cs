dcf36d78753a686e77f7ad6043667949ed990759
devRaGonSa <97627393+devRaGonSa@users.noreply.github.com>
2026-05-27 18:40:28 +0200

    feat(domain): add building type model

---
+++ src/VoidEmpires.Domain/Buildings/BuildingType.cs
@@ -0,0 +1,13 @@
+namespace VoidEmpires.Domain.Buildings;
+
+public enum BuildingType
+{
+    CommandCenter = 1,
+    MetalMine = 2,
+    CrystalMine = 3,
+    GasExtractor = 4,
+    SolarPlant = 5,
+    ResearchLab = 6,
+    Shipyard = 7,
+    DefenseGrid = 8
+}
