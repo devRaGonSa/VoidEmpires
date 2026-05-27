53e97aaa1894f19d14475f1f906ceb5d93855cb6
devRaGonSa <97627393+devRaGonSa@users.noreply.github.com>
2026-05-27 20:08:50 +0200

    test(domain): add resource spending coverage

--- tests/VoidEmpires.Tests/PlanetResourceStockpileDomainTests.cs
+++ tests/VoidEmpires.Tests/PlanetResourceStockpileDomainTests.cs
@@ -55,4 +55,36 @@ public class PlanetResourceStockpileDomainTests
         Assert.True(stockpile.HasAtLeast(ResourceType.Metal, 10));
         Assert.False(stockpile.HasAtLeast(ResourceType.Metal, 26));
     }
+
+    [Fact]
+    public void CanSpendReturnsExpectedAvailability()
+    {
+        var stockpile = PlanetResourceStockpile.Create(Guid.NewGuid());
+        stockpile.Increase(ResourceType.Metal, 100);
+        stockpile.Increase(ResourceType.Crystal, 50);
+
+        Assert.True(stockpile.CanSpend(0, 100, 50, 0));
+        Assert.False(stockpile.CanSpend(0, 101, 50, 0));
+    }
+
+    [Fact]
+    public void SpendSubtractsResources()
+    {
+        var stockpile = PlanetResourceStockpile.Create(Guid.NewGuid());
+        stockpile.Increase(ResourceType.Metal, 100);
+        stockpile.Increase(ResourceType.Crystal, 50);
+
+        stockpile.Spend(0, 60, 15, 0);
+
+        Assert.Equal(40, stockpile.Metal);
+        Assert.Equal(35, stockpile.Crystal);
+    }
+
+    [Fact]
+    public void SpendRejectsInsufficientResources()
+    {
+        var stockpile = PlanetResourceStockpile.Create(Guid.NewGuid());
+
+        Assert.Throws<InvalidOperationException>(() => stockpile.Spend(0, 1, 0, 0));
+    }
 }
