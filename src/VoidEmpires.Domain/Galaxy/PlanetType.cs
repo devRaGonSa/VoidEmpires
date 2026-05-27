905a585989b7c904673f774559f717019955726a
devRaGonSa <97627393+devRaGonSa@users.noreply.github.com>
2026-05-27 11:14:02 +0200

    feat(domain): add planet type enum

---
+++ src/VoidEmpires.Domain/Galaxy/PlanetType.cs
@@ -0,0 +1,12 @@
+namespace VoidEmpires.Domain.Galaxy;
+
+public enum PlanetType
+{
+    Terran = 1,
+    Desert = 2,
+    Ice = 3,
+    Volcanic = 4,
+    Oceanic = 5,
+    Barren = 6,
+    GasGiant = 7
+}
