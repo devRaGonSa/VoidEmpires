6e9a620bade67d9a5e6695660891670d322168a1
devRaGonSa <97627393+devRaGonSa@users.noreply.github.com>
2026-05-31 12:37:37 +0200

    feat(fleets): register orbital travel estimate service

--- src/VoidEmpires.Infrastructure/VoidEmpiresPersistenceServiceCollectionExtensions.cs
+++ src/VoidEmpires.Infrastructure/VoidEmpiresPersistenceServiceCollectionExtensions.cs
@@ -53,6 +53,7 @@ public static class VoidEmpiresPersistenceServiceCollectionExtensions
         services.AddScoped<IOrbitalGroupService, OrbitalStockGroupService>();
         services.AddScoped<IOrbitalGroupLookupService, OrbitalGroupLookupService>();
         services.AddScoped<IOrbitalGroupTransferPlanningService, OrbitalGroupPlannerService>();
+        services.AddScoped<IOrbitalTravelEstimateService, OrbitalTravelEstimateService>();
         services.AddScoped<IOrbitalTransferPersistenceService, OrbitalTransferPersistenceService>();
         services.AddScoped<IOrbitalTransferCompletionService, OrbitalTransferCompletionService>();
         services.AddScoped<IOrbitalTransferLookupService, OrbitalTransferLookupService>();
